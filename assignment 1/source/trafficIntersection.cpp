6f59571d6e04446a1244a1e90ac22eed9713a96d
ArielSixwings <arielserafim@gmail.com>
2022-07-29 19:23:26 -0300

    not tested control intersection

--- assignment 1/source/trafficIntersection.cpp
+++ assignment 1/source/trafficIntersection.cpp
@@ -40,6 +40,54 @@ void trafficIntersection::print(){
 void trafficIntersection::listenButton(int pin){
 	wiringPiIRS(pin,INT_EDGE_RISING,changeTime);
 }
+
 void changeTime(){
 	useTime = minTime;
+}
+
+void trafficIntersection::manageTime(){
+	using namespace std::literals::chrono_literals;
+
+	auto start = std::chrono::high_resolution_clock::now();
+	auto end = std::chrono::high_resolution_clock::now();
+	
+	std::chrono::duration<float> duration = end - start;
+
+	for (int i = 0; duration < std::chrono::duration<float>(useTime); ++i){
+		std::this_thread::sleep_for(0.1s);
+		
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+	}
+
+	useTime = this->maxGreenTime;
+	std::cout << "out of for, returning to proced to new state" << std::endl;
+}
+
+void trafficIntersection::controlIntersection(){
+	this->theSemaphore.setDefaultGroup(1);
+	//changeStates
+	this->listenButton(this->buttonOne);
+	this->listenButton(this->buttonTwo);
+
+	for (int i = 0; i < 100; ++i){
+		this->theSemaphore.changeStates(0);//green 		red
+		this->manageTime();
+		
+		this->theSemaphore.changeStates(1);//yellow 	red
+		sleep(this->yellowTime);
+		
+		this->theSemaphore.changeStates(2);//red 		red
+		sleep(1);
+		
+		this->theSemaphore.changeStates(3);//red 		green
+		sleep(this->minGreenTime);
+
+		this->theSemaphore.changeStates(4);//red 		yellow	
+		sleep(this->yellowTime);
+
+		this->theSemaphore.changeStates(2);//red 		red
+		sleep(1);
+	}
+
 }
