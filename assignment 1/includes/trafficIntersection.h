58e1d1ab4f8a814b94fb1d3ec8de14f3b92a006b
ArielSixwings <arielserafim@gmail.com>
2022-07-30 18:20:02 -0300

    gets velocity corectly

--- assignment 1/includes/trafficIntersection.h
+++ assignment 1/includes/trafficIntersection.h
@@ -7,8 +7,11 @@
 #include <sys/wait.h>
 #include <thread>
 
+static float velocity;
 static int useTime = 10;
 static int minTime = 5;
+static std::chrono::time_point<std::chrono::high_resolution_clock> timeStart;
+static std::chrono::time_point<std::chrono::high_resolution_clock> timeEnd;
 
 class trafficIntersection: public pinMap
 {
@@ -16,14 +19,22 @@ class trafficIntersection: public pinMap
 public:
 	semaphore theSemaphore;
 
+	int velocityOneA;
+	int velocityOneB;
+
+	int velocityTwoA;
+	int velocityTwoB;	
+
 	int buttonOne;
 	int buttonTwo;
+
 	int carSensorOne;
 	int carSensorTwo;
 
 	trafficIntersection();
 
 	void manageTime();
+	void getVelocity(int sensorA, int sensorB);
 	void listenButton(int pin);
 	void controlIntersection();
 
--- assignment 1/source/trafficIntersection.cpp
+++ assignment 1/source/trafficIntersection.cpp
@@ -6,28 +6,57 @@ trafficIntersection::trafficIntersection(){
 
 	this->buttonOne = this->theMap[8];
 	this->buttonTwo = this->theMap[7];
+
 	this->carSensorOne = this->theMap[14];
 	this->carSensorTwo = this->theMap[15];
 
+	this->velocityOneA = this->theMap[23];
+	this->velocityOneB = this->theMap[18];
+	this->velocityTwoA = this->theMap[25];
+	this->velocityTwoB = this->theMap[24];
+	
 	pinMode(this->theMap[8], INPUT);
 	pinMode(this->theMap[7], INPUT);
+	
 	pinMode(this->theMap[14], INPUT);
 	pinMode(this->theMap[15], INPUT);
 
+	pinMode(this->theMap[18], INPUT);
+	pinMode(this->theMap[23], INPUT);
+	pinMode(this->theMap[24], INPUT);
+	pinMode(this->theMap[25], INPUT);
 }
 
 void changeTime(){
 	useTime = minTime;
 }
 
+void getTimeStart(){
+	timeStart = std::chrono::high_resolution_clock::now();
+}
+
+void getTimeEnd(){
+	timeEnd = std::chrono::high_resolution_clock::now();
+	
+	std::chrono::duration<float> duration = timeEnd - timeStart;
+	
+	velocity = 3.6*1/float(duration.count());
+	
+	std::cout << "velocity: "<< velocity << std::endl;
+
+}
+
 void trafficIntersection::listenButton(int pin){
 	wiringPiISR(pin,INT_EDGE_RISING,changeTime);
 }
 
+void trafficIntersection::getVelocity(int sensorA, int sensorB){
+	wiringPiISR(sensorA, INT_EDGE_RISING,getTimeStart);
+	wiringPiISR(sensorB, INT_EDGE_RISING,getTimeEnd);
+}
 
 void trafficIntersection::manageTime(){
 	
-
 	auto start = std::chrono::high_resolution_clock::now();
 	auto end = std::chrono::high_resolution_clock::now();
 	
@@ -52,10 +81,14 @@ void trafficIntersection::controlIntersection(){
 	this->listenButton(this->carSensorTwo);
 
 	this->theSemaphore.changeStates(5);//off 		off
+	
 	std::this_thread::sleep_for(5s);
 	for (int i = 0; i < 100; ++i){
 		std::cout << "Start new iteration" << std::endl;
+		
+
 		this->theSemaphore.changeStates(0);//green 		red
+		this->getVelocity(this->velocityOneA,this->velocityOneB);
 		this->manageTime();
 		
 		this->theSemaphore.changeStates(1);//yellow 	red
