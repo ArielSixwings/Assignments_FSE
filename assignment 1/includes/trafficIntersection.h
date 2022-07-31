c805fe1317c76fbe91eecc62014ddd091a26b672
ArielSixwings <arielserafim@gmail.com>
2022-07-31 16:54:24 -0300

    functional red light infraction

--- assignment 1/includes/trafficIntersection.h
+++ assignment 1/includes/trafficIntersection.h
@@ -7,9 +7,21 @@
 #include <sys/wait.h>
 #include <thread>
 
-static float velocity;
+#define BOTH_ROAD_RED 15
+#define MAIN_ROAD_RED 20
+#define SECONDARY_ROAD_RED 10
+
+static int roadStatus = 0;
+static float velocity = 0.0;
+
+static int totalInfraction = 0;
+static int mainRoadInfraction = 0;
+static int secondaryRoadInfraction = 0;
+
+
 static int useTime = 10;
 static int minTime = 5;
+
 static std::chrono::time_point<std::chrono::high_resolution_clock> timeStart;
 static std::chrono::time_point<std::chrono::high_resolution_clock> timeEnd;
 
@@ -37,6 +49,8 @@ public:
 	void getVelocity(int sensorA, int sensorB);
 	void listenButton(int pin);
 	void controlIntersection();
+	void mainRoadRedLightInfraction(int sensorA, int sensorB);
+	void secondaryRoadRedLightInfraction(int sensorA, int sensorB);
 
 };
 
--- assignment 1/source/trafficIntersection.cpp
+++ assignment 1/source/trafficIntersection.cpp
@@ -2,6 +2,41 @@
 
 using namespace std::literals::chrono_literals;
 
+void changeTime(){
+	useTime = minTime;
+}
+
+void getTimeStart(){
+	timeStart = std::chrono::high_resolution_clock::now();
+}
+
+void checkMainRedLight(){
+	if ((roadStatus == MAIN_ROAD_RED) or (roadStatus == BOTH_ROAD_RED)){
+		mainRoadInfraction++;
+		totalInfraction++;
+	}
+}
+
+void getTimeEnd(){
+	timeEnd = std::chrono::high_resolution_clock::now();
+	
+	std::chrono::duration<float> duration = timeEnd - timeStart;
+	
+	velocity = 3.6*1/float(duration.count());
+	
+	std::cout << "velocity: "<< velocity << std::endl;
+	checkMainRedLight();
+
+}
+
+
+void checkSecondaryRedLight(){
+	if ((roadStatus == SECONDARY_ROAD_RED) or (roadStatus == BOTH_ROAD_RED)){
+		secondaryRoadInfraction++;
+		totalInfraction++;
+	}
+}
+
 trafficIntersection::trafficIntersection(){
 
 	this->buttonOne = this->theMap[8];
@@ -21,38 +56,20 @@ trafficIntersection::trafficIntersection(){
 	pinMode(this->theMap[14], INPUT);
 	pinMode(this->theMap[15], INPUT);
 
-	pinMode(this->theMap[18], INPUT);
 	pinMode(this->theMap[23], INPUT);
-	pinMode(this->theMap[24], INPUT);
+	pinMode(this->theMap[18], INPUT);
+
 	pinMode(this->theMap[25], INPUT);
+	pinMode(this->theMap[24], INPUT);
 }
 
-void changeTime(){
-	useTime = minTime;
-}
-
-void getTimeStart(){
-	timeStart = std::chrono::high_resolution_clock::now();
-}
-
-void getTimeEnd(){
-	timeEnd = std::chrono::high_resolution_clock::now();
-	
-	std::chrono::duration<float> duration = timeEnd - timeStart;
-	
-	velocity = 3.6*1/float(duration.count());
-	
-	std::cout << "velocity: "<< velocity << std::endl;
-
+void trafficIntersection::getVelocity(int sensorA, int sensorB){
+	wiringPiISR(sensorA, INT_EDGE_FALLING,getTimeStart);
+	wiringPiISR(sensorB, INT_EDGE_FALLING,getTimeEnd);
 }
 
 void trafficIntersection::listenButton(int pin){
 	wiringPiISR(pin,INT_EDGE_RISING,changeTime);
-}
-
-void trafficIntersection::getVelocity(int sensorA, int sensorB){
-	wiringPiISR(sensorA, INT_EDGE_RISING,getTimeStart);
-	wiringPiISR(sensorB, INT_EDGE_RISING,getTimeEnd);
 }
 
 void trafficIntersection::manageTime(){
@@ -73,6 +90,16 @@ void trafficIntersection::manageTime(){
 	useTime = 2*minTime;
 }
 
+void trafficIntersection::mainRoadRedLightInfraction(int sensorA, int sensorB){
+	wiringPiISR(sensorA, INT_EDGE_FALLING,checkMainRedLight);
+	wiringPiISR(sensorB, INT_EDGE_FALLING,checkMainRedLight);
+}
+
+void trafficIntersection::secondaryRoadRedLightInfraction(int sensorA, int sensorB){
+	wiringPiISR(sensorA, INT_EDGE_FALLING,checkSecondaryRedLight);
+	wiringPiISR(sensorB, INT_EDGE_FALLING,checkSecondaryRedLight);
+}
+
 void trafficIntersection::controlIntersection(){
 	this->listenButton(this->buttonOne);
 	this->listenButton(this->buttonTwo);
@@ -80,30 +107,44 @@ void trafficIntersection::controlIntersection(){
 	this->listenButton(this->carSensorOne);
 	this->listenButton(this->carSensorTwo);
 
+	this->getVelocity(this->velocityOneA,this->velocityOneB);
+	this->getVelocity(this->velocityTwoA,this->velocityTwoB);
+
+	this->secondaryRoadRedLightInfraction(this->carSensorOne,this->carSensorTwo);
+
+	// this->getVelocity(this->velocityTwoA,this->velocityTwoB);
+	
 	this->theSemaphore.changeStates(5);//off 		off
 	
 	std::this_thread::sleep_for(5s);
 	for (int i = 0; i < 100; ++i){
 		std::cout << "Start new iteration" << std::endl;
+		std::cout << "Main Road Infraction:" << mainRoadInfraction <<std::endl;
+		std::cout << "Secondary Road Infraction:" << secondaryRoadInfraction <<std::endl;
+		std::cout << "Total Infraction:" << totalInfraction <<std::endl;
+
 		
 
 		this->theSemaphore.changeStates(0);//green 		red
-		this->getVelocity(this->velocityOneA,this->velocityOneB);
+		roadStatus = SECONDARY_ROAD_RED;
 		this->manageTime();
 		
 		this->theSemaphore.changeStates(1);//yellow 	red
 		std::this_thread::sleep_for(3s);
 		
 		this->theSemaphore.changeStates(2);//red 		red
+		roadStatus = BOTH_ROAD_RED;
 		std::this_thread::sleep_for(1s);
 		
 		this->theSemaphore.changeStates(3);//red 		green
+		roadStatus = MAIN_ROAD_RED;
 		std::this_thread::sleep_for(5s);
 
 		this->theSemaphore.changeStates(4);//red 		yellow
 		std::this_thread::sleep_for(3s);
 
 		this->theSemaphore.changeStates(2);//red 		red
+		roadStatus = BOTH_ROAD_RED;
 		std::this_thread::sleep_for(1s);
 	}
 
